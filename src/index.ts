62fad64370f1584f9c1739182c8f8ee7abbb451e
Chris Pachomski <cpachomski@gmail.com>
2019-02-27 22:30:11 -0500

    add new interfaces and extractTypes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,88 @@
 import { request } from "graphql-request";
 import query from "./query";
 
+interface QueryArgument {
+  name: String;
+  type: String;
+  kind: String;
+}
+
+interface Query {
+  name: String;
+  description: String;
+  args: [QueryArgument];
+}
+
+const extractQueries = (schemaDef: any): [Query] =>
+  schemaDef.__schema.queryType.fields.map((q: any) => ({
+    name: q.name,
+    description: q.description,
+    args: q.args.map((a: any) => ({
+      name: a.name,
+      type: a.type.name,
+      kind: a.type.kind
+    }))
+  }));
+
+const DefaultGraphQLTypes = [
+  "__Type",
+  "__TypeKind",
+  "__Field",
+  "__InputValue",
+  "__EnumValue",
+  "__Directive",
+  "__DirectiveLocation",
+  "__Schema",
+  "Query",
+  "Boolean",
+  "String",
+  "Int",
+  "Float"
+];
+
+interface TypeAttribute {
+  name: String;
+  description: String;
+  isDepcrecated: Boolean;
+  deprecationReason: String;
+  type: String;
+  kind: String;
+}
+
+interface Type {
+  name: String;
+  attributes: [TypeAttribute];
+}
+
+const extractTypes = (schemaDef: any): [Type] =>
+  schemaDef.__schema.types
+    .filter((f: any) => !DefaultGraphQLTypes.includes(f.name))
+    .map((t: any) => ({
+      name: t.name,
+      attributes: t.fields
+        ? t.fields.map((f: any) => ({
+            name: f.name,
+            description: f.description,
+            isDeprecated: f.isDeprecated,
+            deprecationReason: f.deprecationReason,
+            type: f.type.name,
+            kind: f.type.kind
+          }))
+        : null
+    }));
+
 export async function delve(url: string) {
   try {
     const schema = await request(url, query);
-    return schema;
+    return {
+      queries: extractQueries(schema),
+      types: extractTypes(schema)
+    };
   } catch (err) {
     return console.error(err);
   }
 }
+
+delve("http://content.cnnmoney.ch/graphql").then((data: any) =>
+  console.log(JSON.stringify(data, undefined, 2))
+);
